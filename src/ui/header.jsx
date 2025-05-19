d2cb4c0d5629b761297d3880d304becc6164dee1
salimdotpy <salimdotpy@gmail.com>
2025-05-19 12:04:29 +0100

    new update

--- src/ui/header.jsx
+++ src/ui/header.jsx
@@ -60,7 +60,7 @@ function NavList() {
     return (
         <List className="lg:flex min-w-0 lg:flex-row lg:p-1 lg:px-6">
             {links.map((link, key) =>
-            (link.href.startsWith('/') ?
+            (link.href.startsWith('/#') ?
                 <a href={link.href} key={key} className="text-primary">
                     <ListItem className="flex items-center gap-2 py-3 pr-4">{link.name}</ListItem>
                 </a> :
