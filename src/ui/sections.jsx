a4a802979d3265d8b71782732dfc664083c61e81
salimdotpy <salimdotpy@gmail.com>
2025-05-20 13:47:57 +0100

    new update

--- src/ui/sections.jsx
+++ src/ui/sections.jsx
@@ -46,7 +46,7 @@ export function FormSkeleton({ size = 5, className = ''}) {
 export const HeroSection = () => {
     return (
         <div id="hero" className='h-screen md:h-[80vh] bg-[url(/images/img1.jpeg)] bg-cover bg-fixed bg-[bottom] bg-no-repeat relative'>
-            <div className='absolute flex w-full h-screen md:h-[80vh] from-black via-transparent to-black bg-gradient-to-b'>
+            <div className='absolute flex w-full h-screen md:h-[80vh] from-black via-green-500/70 to-black/30 bg-gradient-to-b'>
                 <div className='container m-auto text-white px-4 md:px-0 text-center'>
                     <Typography variant='h1' className="text-2xl md:text-4xl lg:text-5xl lg:!leading-[1.4] font-normal" data-aos="fade-left" data-aos-delay={100}>
                         Osun State Influencers:<br/>Vote, Engage, and Celebrate!
