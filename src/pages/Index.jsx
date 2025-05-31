e0790d9a3acfada2b7d73bb0b665af69c78a520e
salimdotpy <salimdotpy@gmail.com>
2025-05-31 14:17:20 +0100

    debugging

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AboutSection, CompetitionSection, FactSection, FooterSection, HeroSection, NewSections } from "../ui/sections";
 import { useDocumentTitle } from "@/hooks";
+import Seo from "@/ui/Seo";
 
 export default function Index() {
     useDocumentTitle('InfoTel9ja');
@@ -8,6 +9,7 @@ export default function Index() {
         <>
             {/* <HeroSection /> */}
             {/* <NewSections /> */}
+            <Seo />
             <HeroSection />
             <FactSection />
             <CompetitionSection />
--- src/routers/PublicRoute.jsx
+++ src/routers/PublicRoute.jsx
@@ -1,12 +1,10 @@
 import Header from '@/ui/header';
-import Seo from '@/ui/Seo';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const PublicRoute = () => {
     return (
         <>
-            <Seo />
             <Header />
             <main className="pt-16">
                 <Outlet />
