f04baa945bb71cb578d571ea386e3d12f7982a8b
salimdotpy <salimdotpy@gmail.com>
2025-06-16 12:26:34 +0100

    new update

--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -2,9 +2,12 @@ import { useDocumentTitle } from '@/hooks';
 import useContestantStore from '@/store/contestantStore';
 import useContestStore from '@/store/contestStore';
 import { FooterSection, HeroBreaCrumbs, LoadingComponent } from '@/ui/sections';
-import { InformationCircleIcon } from '@heroicons/react/24/outline';
-import { Alert, Avatar, Badge, Button, Card, CardBody, CardHeader, Chip, Typography } from '@material-tailwind/react';
+import { IWOL } from '@/utils/constants';
+import { DocumentDuplicateIcon, InformationCircleIcon, MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { Alert, Avatar, Badge, Button, Card, CardBody, CardHeader, Chip, IconButton, Input, Tooltip, Typography } from '@material-tailwind/react';
+import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
+import { BiLogoFacebook, BiLogoWhatsapp } from 'react-icons/bi';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
@@ -40,8 +43,9 @@ const Vote = () => {
     return (
         <>
             <HeroBreaCrumbs page={contestant?.fullname || 'Contestant'} links={links} />
-            {!loading && contestant ? <>Comming Soon</> :
+            {!loading && contestant ? <Sections data={contestant} /> :
             <LoadingComponent />
+            // {dayjs().diff(dayjs(contestant.created_at), 'd')}
             }
             <FooterSection />
         </>
@@ -51,77 +55,53 @@ const Vote = () => {
 export default Vote;
 
 const Sections = ({ data = {}}) => {
+    const [quantity, setQuantity] = useState(data?.contest?.minVote || 2);
     const today = new Date();
 
     return (
-        <section id='competition' className='py-10'>
+        <section id='vote' className='py-10'>
             <div className='container xl:w-[90%] mx-auto'>
+                <Card className='bg-header text-fore basis-full lg:basis-1/2 mb-6 mx-4'>
+                    <CardBody className='flex flex-wrap gap-5 *:grow *:basis-[45%]'>
+                        <Avatar src={data?.image || '/images/img4.jpeg'} alt={data?.fullname} variant='rounded' className='h-[250px]' />
+                        <div>
+                            <Typography variant="h5" className="mb-4">{data?.fullname}</Typography>
+                            <labe>Total Gem(s) Aquire:</labe> <br />
+                            <div className='flex items-center jus'>
+                                <Typography variant="h3">{data?.votes || 0}</Typography>
+                                <span>💎</span>
+                            </div>
+                            <div className='flex gap-2 items-center my-4'>
+                                <Button size='sm' variant='outlined' className='flex items-center gap-2 capitalize'>
+                                    <DocumentDuplicateIcon className='size-4' /> Copy Link
+                                </Button>
+                                <Tooltip content='Share via facebook'>
+                                    <IconButton size='sm' variant='outlined'>
+                                        <BiLogoFacebook className='size-5' />
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip content='Share via whatsapp'>
+                                    <IconButton size='sm' variant='outlined'>
+                                        <BiLogoWhatsapp className='size-5' />
+                                    </IconButton>
+                                </Tooltip>
+                            </div>
+                            <labe>Number of votes you want to get:</labe> <br />
+                            <ProductQuantity min={data?.contest?.minVote} getQty={setQuantity} />
+                            <Button type="submit" className={`mt-6 bg-primary disabled:!pointer-events-auto disabled:cursor-not-allowed justify-center sticky bottom-5`}>
+                                Pay ₦{quantity * data?.contest?.votePrice} for {quantity} Gems
+                            </Button>
+                        </div>
+                    </CardBody>
+                </Card>
                 <div className='flex flex-wrap gap-6 px-4 w-full'>
                     <Card className='bg-header text-fore basis-full lg:basis-1/2'>
                         <CardBody>
-                            <Avatar src={data?.contestImage || '/images/img4.jpeg'} alt='competition-image' variant='rounded' className='!size-44 block' />
+                            <Avatar src={data?.contest?.contestImage || '/images/img4.jpeg'} alt='competition-image' variant='rounded' className='!size-44 block' />
                             <Typography variant="h5" className="my-4">
-                                {data?.contestName}
+                                {data?.contest?.contestName}
                             </Typography>
-                            <div className='rsw-editor !border-0' dangerouslySetInnerHTML={{__html: data?.tnc}} />
-                        </CardBody>
-                    </Card>
-                    <Card className='bg-header text-fore basis-full lg:basis-[46%] grow'>
-                        <CardBody>
-                            <Typography variant="h5">Contestants</Typography>
-                            <Alert icon={<InformationCircleIcon className='size-6' />} variant='ghost' color='yellow' className='text-justify my-3 last:*:!mr-3'>
-                                As of today, Thursday, 22 May 2025, 01:01pm, the contestants listed below are the Top Contestants (by votes). Voting is still on. Keep voting to increase the position of your favorite contestants. The Top 3 Contestants wins the prize.
-                            </Alert>
-                            <Typography variant="h6">Top Contestants</Typography>
-                            <div className='flex flex-wrap gap-6 w-full mt-3'>
-                                {data.contestants ? data.contestants.map((contestant, key) => 
-                                <Card key={key} className='flex-1 bg-back min-w-40'  data-aos="fade-up" data-aos-delay={`${key}00`}>
-                                    <CardBody className='flex gap-1.5 flex-col items-center text-center text-fore px-2'>
-                                        <Badge placement="top-end" overlap="circular" content={contestant?.votes || 0} color="green" withBorder>
-                                            <Avatar src={contestant?.image} size='xl' />
-                                        </Badge>
-                                        <Typography variant="h6" className="!line-clamp-2">
-                                            {contestant?.fullname}
-                                        </Typography>
-                                        <Link to={`/vote/${contestant?.id}`}>
-                                            <Button size='sm' className='bg-primary'>Vote</Button>
-                                        </Link>
-                                    </CardBody>
-                                </Card>
-                                ) : <></>}
-                                {/* {[1,2,3,1,3,2,1,3,2,1,3,2].map((item, key) => 
-                                <Card key={key} className='flex-1 bg-back min-w-40'  data-aos="fade-up" data-aos-delay={`${key}00`}>
-                                    <CardBody className='flex gap-1.5 flex-col items-center text-center text-fore px-2'>
-                                        <Badge placement="top-end" overlap="circular" content={item} color="green" withBorder>
-                                            <Avatar src={`/images/img${item}.jpeg`} size='xl' />
-                                        </Badge>
-                                        <Typography variant="h6" className="!line-clamp-2">
-                                            Contestant's Name
-                                        </Typography>
-                                        <Link to={'/competition/18th Edition - Master at Photos Contest'}>
-                                            <Button size='sm' className='bg-primary'>Vote</Button>
-                                        </Link>
-                                    </CardBody>
-                                </Card>
-                                )} */}
-                            </div>
-                        </CardBody>
-                    </Card>
-                    <Card className='bg-header text-fore basis-full lg:basis-[46%] grow hidden'>
-                        <CardBody>
-                            <Typography variant="h5">Winners</Typography>
-                            <div className='flex flex-wrap gap-6 w-full mt-3'>
-                                {[1,2,3,1,3].map((item, key) => 
-                                <Card key={key} className='flex-1 bg-back min-w-40'  data-aos="fade-up" data-aos-delay={`${key}00`}>
-                                    <CardBody className='flex p-3 gap-1.5 flex-col text-fore'>
-                                        <Typography variant="h6" className="!line-clamp-2">
-                                            Contestant's Name
-                                        </Typography>
-                                        <Avatar src={`/images/img${item}.jpeg`} variant='rounded' className='!w-full !h-[150px]' />
-                                    </CardBody>
-                                </Card>
-                                )}
-                            </div>
+                            <div className='rsw-editor !border-0' dangerouslySetInnerHTML={{__html: data?.contest?.tnc}} />
                         </CardBody>
                     </Card>
                 </div>
@@ -129,3 +109,42 @@ const Sections = ({ data = {}}) => {
         </section>
     )
 }
+
+export const ProductQuantity = ({ min = 1, max = Infinity, unitPrice = 1, getTotal = null, getQty = null }) => {
+    const [quantity, setQuantity] = useState(min);
+
+    const total = quantity * unitPrice;
+    const handleTotal = () => {
+        getTotal && getTotal(total);
+        getQty && getQty(quantity);
+    }
+
+    const increment = () => {
+        if (quantity < max) {
+            setQuantity(quantity + 1);
+        }
+    }
+
+    const decrement = () => {
+        if (quantity > min) {
+            setQuantity(quantity - 1);
+        }
+    }
+
+    useEffect(() => {
+        handleTotal()
+    }, [quantity]);
+
+    return (
+        <div className='*:inline-block space-x-3 shrink-0'>
+            <IconButton size='sm' variant='outlined' className="disabled:!cursor-not-allowed disabled:pointer-events-auto !size-7" onClick={decrement} disabled={quantity <= min}>
+                <MinusIcon className='size-4' />
+            </IconButton>
+            <Typography variant='h6'>{quantity}</Typography>
+            {/* <input value={quantity} className='border border-primary focus-visible:!outline-primary/90 rounded-md px-3 py-2'  /> */}
+            <IconButton size='sm' variant='outlined' className="disabled:!cursor-not-allowed disabled:pointer-events-auto !size-7" onClick={increment} disabled={quantity >= max}>
+                <PlusIcon className='size-4' />
+            </IconButton>
+        </div>
+    )
+};
